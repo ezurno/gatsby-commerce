02a241dcf6976211a234a47a88f3eecf76ae0e89
ezurno <ezurno@gmail.com>
2023-02-25 23:23:28 +0900

    LEARN: dynamic-Query (page)
    1. query에 graphql을 하 자체적으로 gatsby가 인식해서 연결 해줌
    
    2. PageProps 자체 인터페이스를사용해 타입을 지정 가능

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,21 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-commerce`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  graphqlTypegen: true, // graphQL 의 data를 자동으로 type 변환 해줌
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog-posts`,
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,30 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Helmet from "../components/Helmet";
 import Layout from "../components/Layout";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.titleDataQuery>) {
+  // PageProps라는 자체 interface를 가져와서 사용
+  // Queries 값은 정적으로 가져올때랑 동일한 방법으로 가져옴
   return (
     <Layout title="Welcome to my blog!">
-      <h1></h1>
+      <ul>
+        {data.allFile.nodes.map((value, index) => (
+          <li key={index}>{value.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query titleData {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`; // 자체적으로 gatsby가 인식해서 연결해 줌
 
 export const Head = () => <Helmet title="BLOG" />;
