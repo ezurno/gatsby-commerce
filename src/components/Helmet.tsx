8ff4645836b48f44b47f4ed4c7bb8b7a3b49023e
ezurno <ezurno@gmail.com>
2023-02-25 22:49:29 +0900

    LEARN: useStaticQuery()
    1. localhost:8000/ 밑의 graghQL을 사용해 정적으로 query의 데이터를 사용 할 수 있음
    
    2. 자동으로 data값을 type 변환을 하기 떄문에 Queries를 사용해 값을 찾으면 type변환 된 interface가 나오며 연결 시켜주면 됨

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
 interface IHelmetProps {
@@ -5,5 +6,24 @@ interface IHelmetProps {
 }
 
 export default function Helmet({ title }: IHelmetProps) {
-  return <title>{title} | DevShop</title>;
+  const data = useStaticQuery<Queries.helmetDataQuery>(graphql`
+    query helmetData {
+      site {
+        siteMetadata {
+          title
+          siteUrl
+          description
+        }
+      }
+    }
+  `); // 정적으로 query 값을 가져오는 함수. localhost:8000의 graphQl 의 값에서 가져올 값을 넣어서 사용
+  // 자동으로  helmetData 값을 type 변환을 하기 때문에 generic type을 찾아보면 type이 있음 가져다 쓰면 됨
+
+  console.log(data);
+
+  return (
+    <title>
+      {title} | {data?.site?.siteMetadata?.title}
+    </title>
+  );
 } // 자동으로 Head에 반환되는 함수 생성
