10aa7a1cd82134f604cc0c5f70e87dd7d6407e7a
ezurno <ezurno@gmail.com>
2023-02-27 01:28:45 +0900

    LEARN: static img
    1. 정적이미지는 StaticImage를 사용해 src에 링크를 걸어줌
    
    2. 사이즈를 직접 맞춰주고 값을 받아서 새로운 값으로 rendering 해주기 때문에 반응형 웹에도 편하게 사용 가능

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import Helmet from "../components/Helmet";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function IndexPage() {
   return (
     <Layout title="Happy Shopping ... ! 👍">
-      <h1></h1>
+      <StaticImage
+        height={300}
+        src="https://images.unsplash.com/photo-1581706537575-8080020a72d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=473&q=80"
+        alt="Sticker Image"
+      />
+      {/* Static으로 img를 넣으면 사이즈를 조절해 주는 기능도 포함 되어 있음 값을 저장하고 새로 rendering 해서 배포 함 */}
     </Layout>
   );
 }
